e0b439871ffc3d68c283becdc17561711d67bbfc
Mr Purple <ash.rawstron@gmail.com>
2019-03-15 20:13:51 -0600

    Add PlayerController.

---
+++ Code/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform tmpDestination;
+
+    private RaycastHit hitPoint;
+
+    private void Start()
+    {
+        // navMeshAgent.SetDestination(tmpDestination.position);
+    }
+
+    private void Update()
+    {
+        if ( ! Input.GetMouseButtonDown(0))
+            return;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPoint, 2000))
+        {
+            navMeshAgent.SetDestination(hitPoint.point);
+        }
+    }
+}
